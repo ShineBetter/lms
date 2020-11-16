34bab32cc0d89573ad8fcd8891fd7acc3ead8d61
amirhack <amirhosseinfalakdin1380@gmail.com>
2020-11-16 13:41:15 +0330

    add cmd.cmd command file

---
+++ cmd.cmd
@@ -0,0 +1,20 @@
+@ECHO OFF
+
+doskey pa=php artisan $*
+doskey pas=php artisan serve $*
+doskey cda=composer dump-autoload $*
+doskey mmodel=php artisan make:model $*
+doskey mcon=php artisan make:Controller $*
+doskey mmigrate=php artisan make:migration $*
+doskey caclear=php artisan cache:clear $*
+doskey confclear=php artisan config:clear $*
+doskey vclear=php artisan view:clear $*
+doskey rlist=php artisan route:list $*
+doskey pam=php artisan module $*
+doskey co=composer $*
+doskey cou=composer update $*
+doskey gb=git branch $*
+doskey gc=git checkout $*
+doskey gs=git status $*
+doskey ga=git add $*
+doskey gcm=git commit $*
