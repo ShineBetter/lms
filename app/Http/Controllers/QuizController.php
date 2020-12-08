fa474053a48a8c940fca37bcd43c660dc3cb21d2
rezagh123 <reza.ghasemiitworld@gmail.com>
2020-12-08 12:56:54 +0330

    add quiz maker

---
+++ app/Http/Controllers/QuizController.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\quiz;
+use Illuminate\Http\Request;
+
+class QuizController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $data=quiz::all();
+        return view('backend.admin.quiz.index',['data' => $data]);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\quiz  $quiz
+     * @return \Illuminate\Http\Response
+     */
+    public function show(quiz $quiz)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\quiz  $quiz
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(quiz $quiz)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\quiz  $quiz
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, quiz $quiz)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\quiz  $quiz
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(quiz $quiz)
+    {
+        //
+    }
+}
---
+++ resources/views/backend/admin/quiz/index.blade.php
@@ -0,0 +1,79 @@
+@extends('backend.admin.partial._master')
+@section('title','آزمون')
+@section('cntd')
+    @parent
+    @php
+        $header="";
+        $loader="dont";
+        $sidebar="";
+        $sidebar="";
+    @endphp
+
+    <div class="dashboard-content-wrap">
+        <div class="container-fluid">
+            <div class="row mt-5">
+                <div class="col-lg-12">
+                    <h3 class="widget-title">آزمون</h3>
+                </div>
+            </div>
+            <div class="row mt-5">
+                <div class="col-lg-12">
+                    <div class="card-box-shared">
+                        <div class="card-box-shared-title">
+                            <x-btn route="level.create"/>
+                        </div>
+                        <div class="card-box-shared-body">
+                            <div class="statement-table purchase-table table-responsive mb-5">
+                                <table class="table">
+                                    <thead>
+                                    <tr>
+                                        <th scope="col">ردیف</th>
+                                        <th scope="col">آزمون</th>
+                                        <th scope="col">عملیات</th>
+                                    </tr>
+                                    </thead>
+                                    <tbody>
+                                    @foreach($data as $key => $item)
+                                        <tr>
+                                            <td scope="row">
+                                                <div class="statement-info">
+                                                    <ul class="list-items">
+                                                        <li class="mb-1">
+                                                            <p>{{ $key + $data->firstItem() }}</p>
+                                                        </li>
+                                                    </ul>
+                                                </div>
+                                            </td>
+                                            <td>
+                                                <div class="statement-info">
+                                                    <ul class="list-items">
+                                                        <li>{{$item->quiz-name}}</li>
+                                                    </ul>
+                                                </div>
+                                            </td>
+                                            <td>
+                                                <div class="statement-info">
+                                                    <ul class="list-items">
+                                                        <li>
+                                                            <a href="{{route('quiz.edit',$item->id)}}"><input
+                                                                    type="button" class="btn btn-info"
+                                                                    style="font-size: 15px;font-family: Tahoma"
+                                                                    value="ویرایش"></a>
+                                                            <x-delbtn route="quiz.destroy" id="{{$item->id}}"/>
+                                                        </li>
+                                                    </ul>
+                                                </div>
+                                            </td>
+                                        </tr>
+                                    @endforeach
+                                    </tbody>
+                                </table>
+                                {{ $data->links() }}
+                            </div>
+                        </div>
+                    </div>
+                </div><!-- end col-lg-12 -->
+            </div><!-- end row -->
+        </div><!-- end container-fluid -->
+    </div><!-- end dashboard-content-wrap -->
+@endsection
